cb5b2bb9de2c76920a8d3a7501041b13fd84489b
maulanasechan <maulanasechan123@gmail.com>
2023-10-31 22:47:33 +0700

    DONE: widget meal item

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal_model.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreeen extends StatelessWidget {
   const MealsScreeen({
@@ -39,7 +40,7 @@ class MealsScreeen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(meals[index].title),
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
       );
     }
 
