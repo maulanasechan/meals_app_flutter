236b33639a58d895b7ce14923cd5debc035a2f06
maulanasechan <maulanasechan123@gmail.com>
2023-10-31 20:10:02 +0700

    first commit

---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MealsScreeen extends StatelessWidget {
+  const MealsScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
